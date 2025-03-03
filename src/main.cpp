4e75129221cb288e97a1f24eee610fb1c044a307
Daan v O <daan.vo@hotmail.com>
2025-03-03 08:21:55 +0100

    Test 4 digits

--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,9 @@
 #include "WemosD1MiniPins.h"
 #include <SPI.h>
 
-#define MOSI    D7
-#define SCK     D5
-#define RCK     D8
+#define MOSI D7
+#define SCK D5
+#define RCK D8
 
 //    A
 //  ┌─────┐
@@ -13,31 +13,33 @@
 // E│     │C
 //  └─────┘
 //     D   DP
-#define SEG_A   (uint8_t)(1 << 7)
-#define SEG_B   (uint8_t)(1 << 6)
-#define SEG_C   (uint8_t)(1 << 3)
-#define SEG_D   (uint8_t)(1 << 1)
-#define SEG_E   (uint8_t)(1 << 0)
-#define SEG_F   (uint8_t)(1 << 5)
-#define SEG_G   (uint8_t)(1 << 4)
-#define SEG_DP  (uint8_t)(1 << 2)
+#define SEG_A (uint8_t)(1 << 7)
+#define SEG_B (uint8_t)(1 << 6)
+#define SEG_C (uint8_t)(1 << 3)
+#define SEG_D (uint8_t)(1 << 1)
+#define SEG_E (uint8_t)(1 << 0)
+#define SEG_F (uint8_t)(1 << 5)
+#define SEG_G (uint8_t)(1 << 4)
+#define SEG_DP (uint8_t)(1 << 2)
 
-#define DIG_1   (uint8_t)(1 << 6)
-#define DIG_2   (uint8_t)(1 << 5)
+#define DIG_1 (uint8_t)(1 << 6)
+#define DIG_2 (uint8_t)(1 << 5)
+#define DIG_3 (uint8_t)(1 << 4)
+#define DIG_4 (uint8_t)(1 << 3)
+#define COLON (uint8_t)(1 << 2)
 
 const uint8_t digitCodes[10] = {
-    (SEG_A | SEG_B | SEG_C | SEG_D | SEG_E | SEG_F),            // 0
-    (SEG_B | SEG_C),                                            // 1
-    (SEG_A | SEG_B | SEG_G | SEG_E | SEG_D),                    // 2
-    (SEG_A | SEG_B | SEG_G | SEG_C | SEG_D),                    // 3
-    (SEG_F | SEG_G | SEG_B | SEG_C),                            // 4
-    (SEG_A | SEG_F | SEG_G | SEG_C | SEG_D),                    // 5
-    (SEG_A | SEG_F | SEG_G | SEG_E | SEG_D | SEG_C),            // 6
-    (SEG_A | SEG_B | SEG_C),                                    // 7
-    (SEG_A | SEG_B | SEG_C | SEG_D | SEG_E | SEG_F | SEG_G),    // 8
-    (SEG_A | SEG_B | SEG_C | SEG_D | SEG_F | SEG_G)             // 9
+    (SEG_A | SEG_B | SEG_C | SEG_D | SEG_E | SEG_F),         // 0
+    (SEG_B | SEG_C),                                         // 1
+    (SEG_A | SEG_B | SEG_G | SEG_E | SEG_D),                 // 2
+    (SEG_A | SEG_B | SEG_G | SEG_C | SEG_D),                 // 3
+    (SEG_F | SEG_G | SEG_B | SEG_C),                         // 4
+    (SEG_A | SEG_F | SEG_G | SEG_C | SEG_D),                 // 5
+    (SEG_A | SEG_F | SEG_G | SEG_E | SEG_D | SEG_C),         // 6
+    (SEG_A | SEG_B | SEG_C),                                 // 7
+    (SEG_A | SEG_B | SEG_C | SEG_D | SEG_E | SEG_F | SEG_G), // 8
+    (SEG_A | SEG_B | SEG_C | SEG_D | SEG_F | SEG_G)          // 9
 };
-
 
 static void update()
 {
@@ -47,7 +49,7 @@ static void update()
 
 static void identifySevenSegment()
 {
-    for (int i=0; i < 8; i++)
+    for (int i = 0; i < 8; i++)
     {
         SPI.transfer(1 << i);
         update();
@@ -77,7 +79,7 @@ void setup()
 
 void loop()
 {
-    for (int i=0; i < 10; i++)
+    for (int i = 0; i < 10; i++)
     {
         for (int k = 0; k < 10; k++)
         {
@@ -85,13 +87,25 @@ void loop()
             {
                 // digit 1
                 SPI.transfer(digitCodes[i]);
-                SPI.transfer(DIG_1);
+                SPI.transfer(DIG_1 | COLON);
                 update();
                 delay(5);
 
                 // digit 2
                 SPI.transfer(digitCodes[k]);
-                SPI.transfer(DIG_2);
+                SPI.transfer(DIG_2 | COLON);
+                update();
+                delay(5);
+
+                // digit 3
+                SPI.transfer(digitCodes[k]);
+                SPI.transfer(DIG_3 | COLON);
+                update();
+                delay(5);
+
+                // digit 4
+                SPI.transfer(digitCodes[k]);
+                SPI.transfer(DIG_4 | COLON);
                 update();
                 delay(5);
             }
@@ -99,5 +113,3 @@ void loop()
     }
     // identifySevenSegment();
 }
-
-
